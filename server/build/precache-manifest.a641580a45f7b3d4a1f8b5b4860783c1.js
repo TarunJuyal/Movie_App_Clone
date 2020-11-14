282eae96e3f8e9c93d2b34b19e40da6af8112cc0
Tarun Juyal <tarunJuyalHere@gmail.com>
2020-11-14 15:57:32 +0530

    new commit

---
+++ server/build/precache-manifest.a641580a45f7b3d4a1f8b5b4860783c1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6ddc6e7da105ab9b062bfaa61010588",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cf243eaebf310326ca9c",
+    "url": "/static/css/2.4c4477be.chunk.css"
+  },
+  {
+    "revision": "f44db67d534b8f749eb4",
+    "url": "/static/css/main.6a59a7ce.chunk.css"
+  },
+  {
+    "revision": "cf243eaebf310326ca9c",
+    "url": "/static/js/2.01bf513a.chunk.js"
+  },
+  {
+    "revision": "4f160e29840e1f64ec2468f8c9029bf3",
+    "url": "/static/js/2.01bf513a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f44db67d534b8f749eb4",
+    "url": "/static/js/main.be8eba64.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "0e4c6b31feadee72442de6770cb9ed67",
+    "url": "/static/media/Logo.0e4c6b31.png"
+  }
+]);
