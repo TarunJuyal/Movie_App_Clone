6d5c90412b3491eef1834abaac5446c9fc603d77
Tarun Juyal <tarunJuyalHere@gmail.com>
2020-11-14 16:14:23 +0530

    new commit

--- server/utils/templates/forgetPasswordMail.js
+++ server/utils/templates/forgetPasswordMail.js
@@ -6,7 +6,7 @@ function forgetPasswordMail(name,resetPasswordToken) {
     You are receiving this mail because you (or someone else) have requested reset of password for your account.
     Please click on the below link or paste thius into your browser to complete the process within 5 minutes of receiving this mail
     
-    Reset link: &{process.env.domain}reset/${resetPasswordToken}
+    Reset link: ${process.env.domain}reset/${resetPasswordToken}
 
     If you didn't request this, please ignore this and your password will remain unchanged.
     </pre>`;
